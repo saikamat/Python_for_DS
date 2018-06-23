1742a8deb1eec426387cbc17c802430fab15e0e2
Sai <sai.s.kamat@gmail.com>
2018-06-23 18:58:22 +0200

    interactive session tensors

---
+++ TF_Basics/script.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+# init. two constant tensors. tensors are arrays.
+x1 = tf.constant([1,2,3,4]);
+x2 = tf.constant([5,6,7,8]);
+
+result = tf.multiply(x1, x2);
+
+sess = tf.Session();
+
+print(sess.run(result));
+sess.close();
+#print(result);
