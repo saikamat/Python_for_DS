b8d53a3db6e8c7ff78bbd4c23a2bba7cc77a7f25
Sai <sai.s.kamat@gmail.com>
2018-06-24 15:33:44 +0200

    test push, inconsequential.

--- TF_Basics/script.py
+++ TF_Basics/script.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 
 # init. two constant tensors. tensors are arrays.
+
+#config = tf.ConfigProto(allow_soft_placement = True);
 x1 = tf.constant([1,2,3,4]);
 x2 = tf.constant([5,6,7,8]);
 
